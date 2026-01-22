0baf87484351c85011b4be46381eaeffaec3fccc
Casa Nuestra <andre@Mac.rsd38.ca>
2026-01-22 10:04:18 -0800

    Fixed returning watilist members

--- waitlist_app.js
+++ waitlist_app.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyARBi6bpk0Z0FCdBIE3ywBe2iPUbiGZZ9E",
-  authDomain: "general-storage-a402e.firebaseapp.com",
-  databaseURL: "https://general-storage-a402e-default-rtdb.firebaseio.com",
-  projectId: "general-storage-a402e",
-  storageBucket: "general-storage-a402e.firebasestorage.app",
-  messagingSenderId: "659108630557",
-  appId: "1:659108630557:web:6aaa6c4d8275ce44d676f4"
+	apiKey: "AIzaSyARBi6bpk0Z0FCdBIE3ywBe2iPUbiGZZ9E",
+	authDomain: "general-storage-a402e.firebaseapp.com",
+	databaseURL: "https://general-storage-a402e-default-rtdb.firebaseio.com",
+	projectId: "general-storage-a402e",
+	storageBucket: "general-storage-a402e.firebasestorage.app",
+	messagingSenderId: "659108630557",
+	appId: "1:659108630557:web:6aaa6c4d8275ce44d676f4"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
@@ -17,21 +17,31 @@ async function addWaitlistPerson() {
 
     if (!discord_username || !florr_username) return;
 
+    let snapshot = await firebase.database().ref("waitlist").once('value');
+    let data = snapshot.val();
+    for (const [key, value] of Object.entries(data || {})) {
+		if (value.discord_username === discord_username) {
+			alert("This member is already on the waitlist.");
+			window.location.href = "admin.html";
+			return;
+		}
+    }
+
     if (is_returning) {
-		await firebase.database().ref("/waitlist/").push({
+      	await firebase.database().ref("/waitlist/").push({
 			discord_username: discord_username, 
 			florr_username: florr_username, 
 			time_added: Date.now(),
 			returning_member: is_returning
-		});
-    } else {
-		await firebase.database().ref("/waitlist/" + Date.now()).set({
+      	});
+	} else {
+      	await firebase.database().ref("/waitlist/" + Date.now()).set({
 			discord_username: discord_username, 
 			florr_username: florr_username, 
 			time_added: Date.now(),
 			returning_member: is_returning
-		});
-	}
+      	});
+    }
 
     document.querySelector("#discord_username_input").value = "";
     document.querySelector("#florr_username_input").value = "";
