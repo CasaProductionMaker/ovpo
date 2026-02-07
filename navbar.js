512192bee0d04918a268ff3541ab9b0093cf13f1
Casa Nuestra <andre@Mac.lan>
2026-02-06 20:02:12 -0800

    Added our story

---
+++ navbar.js
@@ -0,0 +1,9 @@
+const navBarHTML = `<div id="top_bar">
+    <a href="index.html" class="top_bar_item">Home</a>
+    <a href="mission.html" class="top_bar_item">Our Mission</a>
+    <a href="waitlist_system.html" class="top_bar_item">Waitlist</a>
+    <a href="members.html" class="top_bar_item">Members</a>
+    <a href="story.html" class="top_bar_item">Our Story</a>
+</div>`;
+
+document.body.insertAdjacentHTML('beforeend', navBarHTML);
