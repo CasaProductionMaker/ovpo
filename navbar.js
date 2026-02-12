d5adf86380e3d5e5adb3c5074831a17e502e9fe4
Casa Nuestra <andre@Mac.lan>
2026-02-11 20:53:09 -0800

    Made most main pages responsive for mobile.

--- navbar.js
+++ navbar.js
@@ -1,9 +1,9 @@
 const navBarHTML = `<div id="top_bar">
-    <a href="index.html" class="top_bar_item">Home</a>
-    <a href="mission.html" class="top_bar_item">Our Mission</a>
-    <a href="waitlist_system.html" class="top_bar_item">Waitlist</a>
-    <a href="members.html" class="top_bar_item">Members</a>
-    <a href="story.html" class="top_bar_item">Our Story</a>
+    <a href="index.html" class="top_bar_item"><img src="Images/HomeNavbarIcon.svg" class="navbar_icon"><div class="navbar_text">Home</div></a>
+    <a href="mission.html" class="top_bar_item"><img src="Images/MissionNavbarIcon.svg" class="navbar_icon"><div class="navbar_text">Our Mission</div></a>
+    <a href="waitlist_system.html" class="top_bar_item"><img src="Images/WaitlistNavbarIcon.svg" class="navbar_icon"><div class="navbar_text">Waitlist</div></a>
+    <a href="members.html" class="top_bar_item"><img src="Images/MembersNavbarIcon.svg" class="navbar_icon"><div class="navbar_text">Members</div></a>
+    <a href="story.html" class="top_bar_item"><img src="Images/StoryNavbarIcon.svg" class="navbar_icon"><div class="navbar_text">Our Story</div></a>
 </div>`;
 
 document.body.insertAdjacentHTML('beforeend', navBarHTML);
